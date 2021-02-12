1fa28b2dbebc4e461c7e242d38703d93348eeafb
SamakMashwee <74441532+SamakMashwee@users.noreply.github.com>
2021-02-11 18:42:46 -0600

    Delete bot_code directory

--- bot_code/commands_caton/riddle.py
+++
@@ -1,8 +0,0 @@
-
-riddles_answers = {
-	0 : ["What's red and black and white all over?", "A monoracial sacrificial orgy."],
-	1 : ["What is green and fuzzy and will kill you if it falls out of a tree?","pool table."],
-  2: ["What is Paul's favorite word?", "Cummies in my tummies."]
-  
-
-}
